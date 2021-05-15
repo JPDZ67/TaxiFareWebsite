1fd4529b9fc00781bd585a61421d5ec9bad5c621
JM Diaz <jmdiazgorina@gmail.com>
2021-05-15 16:29:09 +0200

    Playing with streamlit app.py

--- app.py
+++ app.py
@@ -2,23 +2,32 @@ import streamlit as st
 from datetime import date, datetime
 import requests
 
+st.set_page_config(layout='wide')
+
 '''
 # Taxi fare prediction challenge
 ## From where to where ?
 '''
 
-st.sidebar.markdown(f"""
-    # Request for a taxi in NY 
-
-    ## When and how many passengers?
-    """)
-
 URL_API_ = "https://taxifareapi-mmmvoa6ccq-ew.a.run.app/predict_fare"
 # URL_API_ = "https://taxifare.lewagon.ai/predict_fare"
 
-pickup_date = st.sidebar.date_input("When do you need the taxi?",date.today())
+cols = st.beta_columns(3)
+
+st.sidebar.markdown(f"""
+    # Request for a taxi in NY 
+
+    ## When do you need a taxi ?
+    """)
+
+pickup_date = st.sidebar.date_input("",date.today())
 pickup_time = st.sidebar.time_input('At which time do you want the pick up?', datetime.now())
-passenger_count = st.sidebar.slider('How many passengers?', 1, 5, 1)
+
+st.sidebar.markdown(f"""
+    ## How many passengers?
+    """)
+
+passenger_count = st.sidebar.slider('', 1, 5, 1)
 
 pickup_datetime = f"{pickup_date} {pickup_time} UTC"
 
@@ -27,12 +36,10 @@ pickup_datetime = f"{pickup_date} {pickup_time} UTC"
 # dropoff_latitude=40.7699
 # dropoff_longitude=-73.9752
 
-cols = st.beta_columns(2)
-
-pickup_latitude = cols[0].st.number_input('Pickup latitude',40.7482)
-pickup_longitude = cols[0].st.number_input('Pickup longitude',-73.985)
-dropoff_latitude = cols[1].st.number_input('Dropoff latitude',40.7482)
-dropoff_longitude = cols[1].st.number_input('Dropoff longitude',-73.9752)
+pickup_latitude = cols[0].number_input('Pickup latitude',40.7482)
+pickup_longitude = cols[0].number_input('Pickup longitude',-73.985)
+dropoff_latitude = cols[1].number_input('Dropoff latitude',40.7482)
+dropoff_longitude = cols[1].number_input('Dropoff longitude',-73.9752)
     
 params_ = {"key":["2013-07-06 17:18:00.000000000"],
             "pickup_datetime": [pickup_datetime],
@@ -43,10 +50,10 @@ params_ = {"key":["2013-07-06 17:18:00.000000000"],
             "passenger_count": [int(passenger_count)]
             }
 
-if st.button('Predict taxi fare',):
+if cols[2].button('Predict taxi fare',):
     res_ = requests.get(URL_API_, params=params_).json()
-    st.write(f"Taxi fare prediction is {round(res_['prediction'],2)} $")
+    cols[2].write(f"{round(res_['prediction'],2)} $")
 
 if st.checkbox('Show params'):
-    params_
+    st.write(params_)
 
